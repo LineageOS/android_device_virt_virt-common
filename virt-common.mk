1efd1b3aa224b951d19b937e9f438e6fc8fb8da8
Vinay Verma <vvinay@codeaurora.org>
2017-11-14 14:37:26 +0530

    virt-common: go: Enable Go specific configuration.
    - Enable dexpreopt for system server and boot image
    - Change heap growth multipier value to 2.0
    - Define low ram flag in msm8937go.mk
    
    [cafebabe: Omit dexpreopt and TARGET_HAS_LOW_RAM]
    
    Change-Id: Ib56b334269874fece9ed3f0fa058413b744fd09a

--- virt-common.mk
+++ virt-common.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
 # Go
 ifeq ($(PRODUCT_IS_GO),true)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults_512.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackageGo.mk)
 endif
 
 # Graphics (Swiftshader)
