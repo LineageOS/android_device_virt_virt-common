95ec61d439364a4d8d8c1454713ce18cd77504e9
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-13 16:58:26 +0800

    [WIP] virt-common: libraries: Implement libgrub_boot_control
    Change-Id: I090c5808246e421e42b92c277025b05211b32482

---
+++ libraries/libgrub_boot_control/include/GrubBootControl.h
@@ -0,0 +1,37 @@
+#include <string>
+
+#include <android-base/unique_fd.h>
+
+#include <libgrub_editenv.h>
+
+namespace libgrub_boot_control {
+
+class GrubBootControl {
+  public:
+    GrubBootControl(std::string grubenv_path_override);
+
+    // android.hardware.boot
+    unsigned int getNumberSlots();
+
+    // Util
+    bool isSlotValid(unsigned int slot);
+
+  protected:
+    std::string GetVarKeyForGlobal(string item);
+    std::string GetItemValueForGlobal(string item);
+    bool SetItemValueForGlobal(string item, string value, bool commit = true);
+
+    std::string GetVarKeyForSlot(unsigned int slot, string item);
+    std::string GetItemValueForSlot(unsigned int slot, string item);
+    bool SetItemValueForSlot(unsigned int slot, string item, string value, bool commit = true);
+    bool SetItemValueForAllSlots(string item, string value, bool commit = true);
+
+  private:
+    ::android::base::unique_fd mFd;
+    ::libgrub_editenv::GrubEnvMap mMap;
+
+    void InitGrubVars();
+    bool CommitGrubVars();
+}
+
+}  // namespace libgrub_boot_control
