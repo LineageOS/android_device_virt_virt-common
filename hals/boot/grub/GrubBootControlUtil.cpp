c738c5ad7867a6a4ef5a310a8b2baf72c1a300bb
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-13 16:58:26 +0800

    [WIP] virt-common: hals: boot: grub: Implement libGrubBootControl
    Change-Id: I090c5808246e421e42b92c277025b05211b32482

---
+++ hals/boot/grub/GrubBootControlUtil.cpp
@@ -0,0 +1,76 @@
+#include <string>
+#include <vector>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <libgrub_editenv.h>
+
+#include <GrubBootControl.h>
+
+using namespace libgrub_editenv;
+
+using std::string;
+using std::vector;
+
+namespace libgrub_boot_control {
+
+namespace {
+
+const string kGrubenvFilePath = "/mnt/vendor/persist/grubenv_abootctrl";
+const string kVarKeyPrefix = "abootctrl_";
+const vector<string> kSlots = {"a", "b"};
+
+}
+
+GrubBootControl::GrubBootControl() {
+    mFd = fd(open(kGrubenvFilePath.c_str(), O_RDWR));
+    CHECK(fd > 0) << "Failed to open " << kGrubenvFilePath;
+    CHECK(LoadFdToMap(mFd, &mMap)) << "Failed to parse " << kGrubenvFilePath;
+}
+
+unsigned int GrubBootControl::getNumberSlots() {
+    return kSlots.size();
+}
+
+bool GrubBootControl::isSlotValid(unsigned int slot) {
+    if (!slot || slot > getNumberSlots()) {
+        return false;
+    }
+    return true;
+}
+
+string GrubBootControl::GetVarKeyForGlobal(string item) {
+    return kVarKeyPrefix + "global_" + item;
+}
+
+string GrubBootControl::GetItemValueForGlobal(string item) {
+    return mMap[GetVarKeyForGlobal(item)];
+}
+
+bool GrubBootControl::SetItemValueForGlobal(string item, string value, bool commit) {
+    mMap.insert_or_assign(GetVarKeyForSlot(item), value);
+    if (commit && !CommitGrubVars()) return false;
+    return true;
+}
+
+string GrubBootControl::GetVarKeyForSlot(unsigned int slot, string item) {
+    string slot_str;
+    if (isSlotValid(slot)) {
+        slot_str = kSlots[slot];
+    } else {
+        slot_str = "UNKNOWN";
+    }
+    return kVarKeyPrefix + "slot_" + slot_str + "_" + item;
+}
+
+string GrubBootControl::GetItemValueForSlot(unsigned int slot, string item) {
+    return mMap[GetVarKeyForSlot(slot, item)];
+}
+
+bool GrubBootControl::SetItemValueForSlot(unsigned int slot, string item, string value, bool commit) {
+    mMap.insert_or_assign(GetVarKeyForSlot(slot, item), value);
+    if (commit && !CommitGrubVars()) return false;
+    return true;
+}
+
+}  // namespace libgrub_boot_control
