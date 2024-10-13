bb1fa9f815f8a6917840e28e798e2f88146713b6
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-13 16:58:26 +0800

    [WIP] virt-common: hals: boot: grub: Implement libGrubBootControl
    Change-Id: I090c5808246e421e42b92c277025b05211b32482

---
+++ hals/boot/grub/include/GrubBootControl.h
@@ -0,0 +1,31 @@
+#include <string>
+
+#include <android-base/unique_fd.h>
+
+#include <libgrub_editenv.h>
+
+namespace libgrub_boot_control {
+
+class GrubBootControl {
+  public:
+    GrubBootControl();
+
+    unsigned int getNumberSlots();
+
+    bool isSlotValid(unsigned int slot);
+
+  protected:
+    std::string GetVarKeyForGlobal(string item);
+    std::string GetItemValueForGlobal(string item);
+    bool SetItemValueForGlobal(string item, string value, bool commit = true);
+
+    std::string GetVarKeyForSlot(unsigned int slot, string item);
+    std::string GetItemValueForSlot(unsigned int slot, string item);
+    bool SetItemValueForSlot(unsigned int slot, string item, string value, bool commit = true);
+
+  private:
+    ::android::base::unique_fd mFd;
+    ::libgrub_editenv::GrubEnvMap mMap;
+}
+
+}  // namespace libgrub_boot_control
