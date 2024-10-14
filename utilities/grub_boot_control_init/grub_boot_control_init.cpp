35b303d491d280f29937104260d8cf8a88623448
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-14 22:15:36 +0800

    virt-common: utilities: Implement grub_boot_control_init
    * For creating initial grubenv file
    
    Change-Id: Iadf7c42f161ffebf1d59fc3265258a5213725803

---
+++ utilities/grub_boot_control_init/grub_boot_control_init.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <GrubBootControl.h>
+
+using namespace libgrub_boot_control;
+
+using std::cout;
+using std::string;
+
+const string kHelpText =
+    "Usage: grub_boot_control_init <path>\n"
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        cout << kHelpText;
+        return 1;
+    }
+
+    GrubBootControl* g = new GrubBootControl(string(argv[1]));
+    g->PrintGrubVars();
+    delete g;
+    return 0;
+}
