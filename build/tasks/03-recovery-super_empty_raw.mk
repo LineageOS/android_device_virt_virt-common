3d02f57bff78fb4b2b47e2679d45223c268892bb
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-12 02:50:23 +0800

    virt-common: Split custom build rules into build/tasks/*.mk
    Change-Id: I6814282fc849ade88386533616b1a023c44222a8

---
+++ build/tasks/03-recovery-super_empty_raw.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(USES_DEVICE_VIRT_VIRT_COMMON),true)
+
+# Super image (empty)
+LPFLASH := $(HOST_OUT_EXECUTABLES)/lpflash$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_RECOVERY_SUPERIMAGE_EMPTY_RAW_TARGET := $(PRODUCT_OUT)/recovery/root/system/etc/super_empty_raw.img
+$(INSTALLED_RECOVERY_SUPERIMAGE_EMPTY_RAW_TARGET): $(LPFLASH) $(PRODUCT_OUT)/super_empty.img
+	touch $@
+	$(LPFLASH) $$(realpath $@) $(PRODUCT_OUT)/super_empty.img
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_RECOVERY_SUPERIMAGE_EMPTY_RAW_TARGET)
+
+endif # USES_DEVICE_VIRT_VIRT_COMMON
