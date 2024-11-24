ee6e0583c7378317c8ec7f9a7e26b7b86f31982d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-25 05:06:41 +0800

    virt-common: create_partition_table.sh: Do not recreate partition table
    * Recreating partition table fails after switching to vAB for unknown reason
    
    Change-Id: Ib83c07d7ba405213af1c614e21f3f0b838a9b4aa

--- configs/scripts/create_partition_table.sh
+++ configs/scripts/create_partition_table.sh
@@ -10,6 +10,10 @@ if [ ! -x "$SGDISK_EXEC" ] || [ ! -w "$TARGET" ] || [ -z "$DISK_NAME" ]; then
     exit 1
 fi
 
+if [ -e "${TARGET}2" ]; then
+    exit 0
+fi
+
 case "$DISK_NAME" in
     "sda"|"vda")
         $SGDISK_EXEC --zap-all $TARGET
