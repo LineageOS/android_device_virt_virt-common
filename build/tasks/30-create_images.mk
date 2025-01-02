83920b71769a2f55ac909a3ad69da0d228624c40
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-02 17:28:14 +0800

    virt-common: build: Switch to $(HOST_OUT_EXECUTABLES)/mtools
    Change-Id: Ie314079e2bf8cb18a75bbbf16f431d5466c8546f

--- build/tasks/30-create_images.mk
+++ build/tasks/30-create_images.mk
@@ -14,8 +14,8 @@ define create-fat32image
 	[ $(4) ] && [ $(4) -gt 0 ] && img_size=$(4) || \
 		img_size=$$($(VIRT_COMMON_PATH)/bootmgr/.calc_fat32_img_size.sh $(2)); \
 		/bin/dd if=/dev/zero of=$(1) bs=1M count=$$img_size
-	$(BOOTMGR_TOOLS_BIN_DIR)/mformat -F -i $(1) -v "$(3)" ::
-	$(foreach content,$(2),$(BOOTMGR_TOOLS_BIN_DIR)/mcopy -i $(1) -s $(content) :: &&)true
+	$(HOST_OUT_EXECUTABLES)/mtools -c mformat -F -i $(1) -v "$(3)" ::
+	$(foreach content,$(2),$(HOST_OUT_EXECUTABLES)/mcopy -i $(1) -s $(content) :: &&)true
 endef
 
 endif # USES_DEVICE_VIRT_VIRT_COMMON
