0cd1577576453947fdcca3009a7d965cd4831050
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-23 22:56:33 +0800

    virt-common: build: Change indents to spaces for non build recipes
    Change-Id: I3298041c614a4e84f672f19d66ae1884f4b38f9d

--- build/tasks/04-kernel_version.mk
+++ build/tasks/04-kernel_version.mk
@@ -7,17 +7,17 @@
 ifeq ($(USES_DEVICE_VIRT_VIRT_COMMON),true)
 
 ifneq ($(wildcard $(TARGET_KERNEL_SOURCE)/Makefile),)
-	LOCAL_KERNEL_VERSION_DISPLAY_PREFIX := Kernel version
+    LOCAL_KERNEL_VERSION_DISPLAY_PREFIX := Kernel version
 else ifneq ($(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/kernel),)
-	LOCAL_KERNEL_VERSION_DISPLAY_PREFIX := Prebuilt kernel version
-	ifneq ($(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/.kernel_version.txt),)
-		LOCAL_KERNEL_VERSION := $(shell cat $(TARGET_PREBUILT_KERNEL_DIR)/.kernel_version.txt)
-	else
-		LOCAL_KERNEL_VERSION := $(TARGET_PREBUILT_KERNEL_USE)
-	endif
+    LOCAL_KERNEL_VERSION_DISPLAY_PREFIX := Prebuilt kernel version
+    ifneq ($(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/.kernel_version.txt),)
+        LOCAL_KERNEL_VERSION := $(shell cat $(TARGET_PREBUILT_KERNEL_DIR)/.kernel_version.txt)
+    else
+        LOCAL_KERNEL_VERSION := $(TARGET_PREBUILT_KERNEL_USE)
+    endif
 else
-	LOCAL_KERNEL_VERSION_DISPLAY_PREFIX := Emulator kernel version
-	LOCAL_KERNEL_VERSION := $(TARGET_PREBUILT_EMULATOR_KERNEL_USE)
+    LOCAL_KERNEL_VERSION_DISPLAY_PREFIX := Emulator kernel version
+    LOCAL_KERNEL_VERSION := $(TARGET_PREBUILT_EMULATOR_KERNEL_USE)
 endif
 
 INSTALLED_KERNEL_VERSION_TXT_TARGET := $(PRODUCT_OUT)/.kernel_version.txt
