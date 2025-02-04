495288e8222f93b94594c0e84556dfd5fdf04e80
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-04 15:39:53 +0800

    [DNM] virt-common: libinit: Remove unused stuff from libinit_variant
    Change-Id: Id57bcadb1a250cb68f8dfa987ef72687f771a409

--- libraries/libinit/libinit_variant.cpp
+++ libraries/libinit/libinit_variant.cpp
@@ -4,29 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <android-base/logging.h>
-#include <android-base/properties.h>
 #include <libinit_utils.h>
-
 #include <libinit_variant.h>
 
-using android::base::GetProperty;
-
-#define HWC_PROP "ro.boot.hwc"
-#define SKU_PROP "ro.boot.product.hardware.sku"
-
-void search_variant(const std::vector<variant_info_t> variants) {
-    std::string hwc_value = GetProperty(HWC_PROP, "");
-    std::string sku_value = GetProperty(SKU_PROP, "");
-
-    for (const auto& variant : variants) {
-        if ((variant.hwc_value == "" || variant.hwc_value == hwc_value) &&
-            (variant.sku_value == "" || variant.sku_value == sku_value)) {
-            set_variant_props(variant);
-            break;
-        }
-    }
-}
+#include <unistd.h>
 
 void set_variant_props(const variant_info_t variant) {
     // Older devices don't have marketname
@@ -36,7 +17,6 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("device", variant.device, true);
     set_ro_build_prop("marketname", marketname, true);
     set_ro_build_prop("model", variant.model, true);
-    property_override("vendor.usb.product_string", marketname, true);
 
     if (access("/system/bin/recovery", F_OK) != 0) {
         property_override("bluetooth.device.default_name", marketname, true);
@@ -45,7 +25,4 @@ void set_variant_props(const variant_info_t variant) {
 
         property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
     }
-
-    if (variant.nfc)
-        property_override(SKU_PROP, "nfc");
 }
