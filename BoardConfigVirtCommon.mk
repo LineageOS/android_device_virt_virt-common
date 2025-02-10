479871533524432e802aaf03ffeed6fd2c13705b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-10 22:58:45 +0800

    virt-common: Make prebuilt kernel load the same modules as source built kernel
    * Otherwise, prebuilt kernel loads all the modules by default, which
      will break functionality in some cases (i.e. multiple DRM drivers).
    
    Change-Id: I2fe8e113e082d3055d19b69574c70cbcab2a1a6b

--- BoardConfigVirtCommon.mk
+++ BoardConfigVirtCommon.mk
@@ -48,11 +48,14 @@ BOARD_KERNEL_CMDLINE := \
     rw \
     androidboot.verifiedbootstate=orange
 
-ifneq ($(wildcard $(TARGET_KERNEL_SOURCE)/Makefile),)
+ifeq ($(EMULATOR_KERNEL_FILE),)
 BOARD_VENDOR_KERNEL_MODULES_LOAD := \
     btusb.ko \
     cfg80211.ko \
     virt_wifi.ko
+endif
+
+ifneq ($(wildcard $(TARGET_KERNEL_SOURCE)/Makefile),)
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     lineageos/peripheral/bluetooth.config \
