f49b98d9617d73e56e1a0b78382b1ec19d838d32
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-25 15:46:40 +0800

    virt-common: tablet2multitouch: Rename functions to report input events
    Change-Id: I2d2d9a710f9bf491d9a1ceb3ddaba931bf770cc9

--- libraries/libtablet2multitouch/include/libtablet2multitouch.h
+++ libraries/libtablet2multitouch/include/libtablet2multitouch.h
@@ -16,9 +16,9 @@ extern int libtablet2multitouch_setup_uinput_device(int* uinput_fd,
 extern void libtablet2multitouch_send_input_event(int uinput_fd, __u16 type, __u16 code,
                                                   __s32 value);
 // Function to send multitouch events
-extern void libtablet2multitouch_send_multitouch_event(int uinput_fd, bool pressed, int tracking_id,
-                                                       __s32 x, __s32 y);
+extern void libtablet2multitouch_report_multitouch(int uinput_fd, bool pressed, int tracking_id,
+                                                   __s32 x, __s32 y);
 // Function to send key events
-extern void libtablet2multitouch_send_key_event(int uinput_fd, __u16 code, __s32 value);
+extern void libtablet2multitouch_report_key(int uinput_fd, __u16 code, __s32 value);
 // Function to handle tablet to multitouch and key translation
 extern void libtablet2multitouch_handle_event(int uinput_fd, struct input_event* ev);
