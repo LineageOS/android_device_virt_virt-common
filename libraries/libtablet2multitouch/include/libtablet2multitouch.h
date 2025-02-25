f5e1c06b2e7ea3c2da56e1af2d0423f75d0c91d9
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-25 20:30:16 +0800

    virt-common: tablet2multitouch: Report hovering events
    * We have mouse cursor that way \o/
    
    Change-Id: I05eebb244197b2a78cb85b61fa6ee0d605ae3111

--- libraries/libtablet2multitouch/include/libtablet2multitouch.h
+++ libraries/libtablet2multitouch/include/libtablet2multitouch.h
@@ -16,8 +16,8 @@ extern int libtablet2multitouch_setup_uinput_device(int* uinput_fd,
 extern void libtablet2multitouch_send_input_event(int uinput_fd, __u16 type, __u16 code,
                                                   __s32 value);
 // Function to send multitouch events
-extern void libtablet2multitouch_report_multitouch(int uinput_fd, bool pressed, int tracking_id,
-                                                   __s32 x, __s32 y);
+extern void libtablet2multitouch_report_multitouch(int uinput_fd, bool active, bool contact,
+                                                   int tracking_id, __s32 x, __s32 y);
 // Function to send key events
 extern void libtablet2multitouch_report_key(int uinput_fd, __u16 code, __s32 value);
 // Function to handle tablet to multitouch and key translation
