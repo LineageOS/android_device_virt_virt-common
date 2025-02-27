fe7a0783c524bab6b691374463c2e8357d9dc6ba
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-28 02:42:33 +0800

    virt-common: Make sure freeform window management enablement is in /product
    * Some system images does not work well with it, and UI feature
      should not be enabled by vendor image in theory
    
    Change-Id: Ibde41935c93946b52b68c8dcfb0c401096875b64

--- virt-common.mk
+++ virt-common.mk
@@ -190,6 +190,9 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(VIRT_COMMON_PATH)/overlays/overlay
 
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(VIRT_COMMON_PATH)/overlays/product_overlay
+
 ifeq ($(PRODUCT_IS_GO),true)
 DEVICE_PACKAGE_OVERLAYS += \
     $(VIRT_COMMON_PATH)/overlays/overlay-go
@@ -233,7 +236,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/go_handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/go_handheld_core_hardware.xml
 else
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/device_virt-android.software.freeform_window_management.xml \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
 endif
 
