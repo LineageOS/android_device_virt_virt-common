1c5b4437fb5d7491f96554a14b48b8cea7958125
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-06 18:19:39 +0800

    virt-common: Use prebuilt mesa_clc starting from Mesa 25
    * Since we don't have libclc library in Android tree
    
    Change-Id: I5a36f738d863455b04d1087ca8689578983e12b6

--- BoardConfigVirtCommon.mk
+++ BoardConfigVirtCommon.mk
@@ -46,6 +46,17 @@ endif
 # Graphics (Mesa)
 BOARD_MESA3D_USES_MESON_BUILD := true
 
+MESA_VERSION_STRING := $(shell cat external/mesa/VERSION)
+MESA_VERSION_MAJOR := $(shell echo "$(MESA_VERSION_STRING)" | cut -d '.' -f 1)
+MESA_VERSION_MINOR := $(shell echo "$(MESA_VERSION_STRING)" | cut -d '.' -f 2)
+MESA_VERSION_PATCH_PRE := $(shell echo "$(MESA_VERSION_STRING)" | cut -d '.' -f 3)
+MESA_VERSION_PATCH := $(shell echo "$(MESA_VERSION_PATCH_PRE)" | cut -d '-' -f 1)
+MESA_VERSION_PRE_RELEASE := $(shell echo "$(MESA_VERSION_PATCH_PRE)" | cut -d '-' -f 2)
+
+ifeq ($(shell expr $(MESA_VERSION_MAJOR) \>= 25), 1)
+BOARD_MESA3D_MESON_ARGS := -Dmesa-clc=system
+endif
+
 ifneq ($(wildcard external/llvm-project/Android.bp),)
 BUILD_BROKEN_PLUGIN_VALIDATION := \
     soong-llvm12 \
