a78732f6ed56d285a88e86f6f73f8724ef1579af
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-12 22:57:43 +0800

    Revert "virt-common: Switch to com.google.cf.bt"
    This reverts commit 19f3a5976c811a634172041771fadae0d0e727ec.
    
    Change-Id: Iae25481d8a70c9c7a0596e07e8b25ccaf57adf45

--- virt-common.mk
+++ virt-common.mk
@@ -60,7 +60,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    com.google.cf.bt
+    android.hardware.bluetooth-service.default
 
 ifneq ($(PRODUCT_IS_ATV),true)
 ifneq ($(PRODUCT_IS_AUTOMOTIVE),true)
@@ -228,6 +228,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.prebuilt.xml \
+    android.hardware.bluetooth_le.prebuilt.xml \
     android.hardware.ethernet.prebuilt.xml \
     android.hardware.usb.host.prebuilt.xml \
     android.hardware.wifi.prebuilt.xml \
