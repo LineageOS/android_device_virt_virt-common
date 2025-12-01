67b7d16743ff4a94c1fc949c2775f90256b91ab7
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-02 03:11:22 +0800

    virt-common: tablet2multitouch: Add licence headers
    Change-Id: I57d015610aa4a9ed4655035670b3d6b3bec52151

--- libraries/libtablet2multitouch/include/libtablet2multitouch.h
+++ libraries/libtablet2multitouch/include/libtablet2multitouch.h
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #pragma once
 
 #include <linux/input.h>
--- services/tablet2multitouch/tablet2multitouch.c
+++ services/tablet2multitouch/tablet2multitouch.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/input.h>
